c8155ff90808ed85f9bd49b194f4409f898f0429
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2020-12-04 18:10:13 +0100

    Add glfw (submodule) and glad
    Also make a minimum project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void glfw_error(int error, const char *description);
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	if (!glfwInit()) {
+		std::cerr << "Init crashed!\n";
+		glfwTerminate();
+		return 1;
+	}
+
+	glfwSetErrorCallback(glfw_error);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow *window = glfwCreateWindow(200, 200, "Test", nullptr, nullptr);
+	if(!window) {
+		std::cerr << "Window could not be initialized!\n";
+		glfwTerminate();
+		return 1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "GLAD could not be initialized!\n";
+		glfwTerminate();
+		return 1;
+	}
+
+	while(!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
 	return 0;
+}
+
+void glfw_error(int error, const char *description){
+	std::cerr << "GLFW Error " << error << ": " <<  description << '\n';
 }
