69a8679560b4cb6595d650304f2566a967226874
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2020-12-08 12:33:20 +0100

    Add imgui

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
 void glfw_error(int error, const char *description);
 
 int main() {
@@ -17,13 +21,14 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow *window = glfwCreateWindow(200, 200, "Test", nullptr, nullptr);
+	GLFWwindow *window = glfwCreateWindow(1280, 720, "Test", nullptr, nullptr);
 	if(!window) {
 		std::cerr << "Window could not be initialized!\n";
 		glfwTerminate();
 		return 1;
 	}
 	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 
 	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cerr << "GLAD could not be initialized!\n";
@@ -31,7 +36,25 @@ int main() {
 		return 1;
 	}
 
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+
+	ImGuiIO& io = ImGui::GetIO();
+	(void) io;
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplOpenGL3_Init("#version 460 core");
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+
 	while(!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		ImGui::ShowDemoWindow();
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
